ec2c9a5453436e5a2429d58dcc8f4c9b2498ef36
Henrique Luís Oliveira Marques <henrique.marques@caf.io>
2024-01-22 16:56:09 -0300

    feat: change

--- react-native.js
+++ react-native.js
@@ -1,40 +1,133 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
+    es6: true,
+  },
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 
   settings: {
     react: {
       version: "detect",
     },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
-    },
   },
 
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+  plugins: [
+    "eslint-comments",
+    "import",
+    "promise",
+    "prettier",
+    "react",
+    "react-hooks",
+    "react-native",
   ],
 
-  plugins: ["react", "jsx-a11y", "react-native", "@typescript-eslint"],
-
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+  extends: [
+    "eslint:recommended",
+    "plugin:eslint-comments/recommended",
+    "plugin:promise/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
 
   rules: {
+    eqeqeq: ["error", "smart"],
+
+    "array-callback-return": "error",
+    "default-case": ["error", { commentPattern: "^no default$" }],
+    "new-parens": "error",
+    "no-array-constructor": "error",
+    "no-caller": "error",
+    "no-case-declarations": "error",
+    "no-cond-assign": ["error", "except-parens"],
+    "no-constant-binary-expression": "error",
+    "no-constructor-return": "error",
+    "no-delete-var": "error",
+    "no-empty": "error",
+    "no-eval": "error",
+    "no-extend-native": "error",
+    "no-extra-bind": "error",
+    "no-extra-boolean-cast": "error",
+    "no-extra-label": "error",
+    "no-extra-semi": "error",
+    "no-func-assign": "error",
+    "no-global-assign": "error",
+    "no-implied-eval": "error",
+    "no-iterator": "error",
+    "no-label-var": "error",
+    "no-labels": ["error", { allowLoop: true, allowSwitch: false }],
+    "no-lone-blocks": "error",
+    "no-loop-func": "error",
+    "no-multi-str": "error",
+    "no-new-func": "error",
+    "no-new-object": "error",
+    "no-new-wrappers": "error",
+    "no-octal-escape": "error",
+    "no-octal": "error",
+    "no-promise-executor-return": "error",
+    "no-redeclare": "error",
+    "no-regex-spaces": "error",
+    "no-self-compare": "error",
+    "no-shadow-restricted-names": "error",
+    "no-template-curly-in-string": "error",
+    "no-throw-literal": "error",
+    "no-unused-labels": "error",
+    "no-unused-private-class-members": "error",
+    "no-use-before-define": "error",
+    "no-useless-computed-key": "error",
+    "no-useless-concat": "error",
+    "no-useless-constructor": "error",
+    "no-useless-escape": "error",
+    "no-useless-rename": "error",
+    "no-with": "error",
+    "require-atomic-updates": "error",
+    "require-yield": "error",
+    "unicode-bom": "error",
+
+    "eslint-comments/disable-enable-pair": ["error", { allowWholeFile: true }],
+    "eslint-comments/no-unused-disable": "error",
+
+    "import/export": "error",
+    "import/extensions": ["error", { js: "never", json: "always" }],
+    "import/imports-first": "error",
+    "import/no-amd": "error",
+    "import/no-commonjs": "error",
+    "import/no-duplicates": "error",
+    "import/no-extraneous-dependencies": "error",
+    "import/no-self-import": "error",
+    "import/no-unresolved": "error",
+
+    "react/button-has-type": "error",
+    "react/no-array-index-key": "error",
+    "react/no-access-state-in-setstate": "error",
+    "react/no-danger": "error",
+    "react/no-did-mount-set-state": "error",
+    "react/no-did-update-set-state": "error",
+    "react/no-redundant-should-component-update": "error",
+    "react/no-this-in-sfc": "error",
+    "react/no-typos": "error",
+    "react/no-unused-prop-types": "error",
+    "react/no-will-update-set-state": "error",
+    "react/prefer-es6-class": "error",
     "react/self-closing-comp": "error",
+    "react/sort-comp": "error",
+    "react/style-prop-object": "error",
+    "react/void-dom-elements-no-children": "error",
+    "react/jsx-boolean-value": ["error", "never"],
+    "react/jsx-curly-brace-presence": ["error", "never"],
+    "react/jsx-pascal-case": "error",
+
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+
+    "react-native/no-unused-styles": "error",
+
     "prettier/prettier": [
       "error",
       {
@@ -47,22 +140,107 @@ module.exports = {
         endOfLine: "auto",
       },
     ],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "jsx-a11y/alt-text": [
-      "warn",
-      {
-        elements: ["img"],
-        img: ["Image"],
-      },
-    ],
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
-    "react/no-unknown-property": "error",
   },
 
-  ignorePatterns: ["node_modules"],
+  overrides: [
+    {
+      files: ["*.js", "*.jsx"],
+      extends: ["plugin:import/recommended"],
+      rules: {
+        "import/default": "error",
+        "import/named": "error",
+        "import/namespace": "error",
+        "import/no-named-as-default": "error",
+        "import/no-named-as-default-member": "error",
+        "import/no-cycle": "error",
+        "import/no-deprecated": "error",
+      },
+    },
+    {
+      files: ["*.mjs"],
+      rules: {
+        "import/extensions": ["error", "always"],
+      },
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: require.resolve("@typescript-eslint/parser"),
+      plugins: ["@typescript-eslint/eslint-plugin"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+      ],
+      rules: {
+        "@typescript-eslint/adjacent-overload-signatures": "error",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/consistent-type-assertions": [
+          "error",
+          { assertionStyle: "as" },
+        ],
+        "@typescript-eslint/member-delimiter-style": "error",
+        "@typescript-eslint/no-dynamic-delete": "error",
+        "@typescript-eslint/no-empty-interface": [
+          "error",
+          { allowSingleExtends: true },
+        ],
+        "@typescript-eslint/no-extra-non-null-assertion": "error",
+        "@typescript-eslint/no-extraneous-class": "error",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-use-before-define": "error",
+        "@typescript-eslint/no-useless-constructor": "error",
+        "@typescript-eslint/prefer-for-of": "error",
+        "@typescript-eslint/prefer-function-type": "error",
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/unified-signatures": "error",
+
+        "default-case": "off",
+        "no-dupe-class-members": "off",
+        "no-undef": "off",
+        "no-use-before-define": "off",
+
+        "react/prop-types": "off",
+      },
+    },
+    {
+      files: ["*.{spec,test}.{js,ts,tsx}", "**/__tests__/**/*.{js,ts,tsx}"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      env: {
+        jest: true,
+        "jest/globals": true,
+      },
+      rules: {
+        "import/no-extraneous-dependencies": [
+          "error",
+          { devDependencies: true },
+        ],
+
+        "jest/consistent-test-it": ["error", { fn: "it" }],
+        "jest/expect-expect": "error",
+        "jest/no-disabled-tests": "error",
+        "jest/no-duplicate-hooks": "error",
+        "jest/no-test-prefixes": "error",
+        "jest/no-test-return-statement": "error",
+        "jest/prefer-to-be": "error",
+        "jest/prefer-todo": "error",
+        "jest/require-to-throw-message": "error",
+      },
+    },
+    {
+      files: ["*.config.js", ".*rc.js"],
+      env: {
+        node: true,
+      },
+      rules: {
+        "import/no-commonjs": "off",
+        "import/no-extraneous-dependencies": [
+          "error",
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
 };
